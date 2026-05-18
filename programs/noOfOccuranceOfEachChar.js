b9efa4af8f8f4ba2be5fc8063bf43a0a32d0df35
Pradeep R <rpradee666@gmail.com>
2026-05-18 17:59:29 +0530

    function for noOfOccuranceOfChar

--- programs/noOfOccuranceOfEachChar.js
+++ programs/noOfOccuranceOfEachChar.js
@@ -18,3 +18,39 @@ for(let uniqueEle of uniqueArrayElements){
   }
   console.log(`${uniqueEle} = ${count}`)
 }
+
+
+//or
+
+
+
+
+//typescript with function
+ 
+// let s = "malayalam";
+
+// let noOfOccuranceOfChar = (str:string) => {
+
+// let map = new Map<string|unknown,number|null>();
+// let set = new Set();
+// for(let i=0; i<s.length; i++){
+//     set.add(s[i]);
+// }
+
+// let uniquechars = [...set]
+
+// for(let uniqueChar of uniquechars){
+//     let count =0;
+//     for(let i=0; i<s.length; i++){
+//         if(uniqueChar===s[i]){
+//             count++;
+//         }
+//     }
+//     console.log(uniqueChar+" = "+count);
+//     map.set(uniqueChar,count);
+// }
+// return map;
+
+// }
+
+// console.log(noOfOccuranceOfChar(s))
--- programs/primeNumber.js
+++ programs/primeNumber.js
@@ -25,15 +25,15 @@ const isPrimeNumber = (num)=>{
   }
 }
 
-console.log(isPrimeNumber(8))
+console.log(isPrimeNumber(9))
 
 //or
 
-function isPrime(n) {
-  if (n < 2) return false;
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) return false;
-  }
-  return true;
-}
-console.log(isPrime(7)); // true
+// function isPrime(n) {
+//   if (n < 2) return false;
+//   for (let i = 2; i <= Math.sqrt(n); i++) {
+//     if (n % i === 0) return false;
+//   }
+//   return true;
+// }
+// console.log(isPrime(7)); // true
